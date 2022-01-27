8a87acef82f5153b7ab5b908ed80c2804c82f722
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-01-28 03:33:18 +0400

    Add main header.

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Places from '@app/ui/places/pages/Places';
+import MainNavigation from '@app/ui/shared/components/navigation/MainNavigation';
 import Users from '@app/ui/users/pages/Users';
 
 function App(): JSX.Element {
   return (
     <div>
-      <h1>Let&apos;s start!</h1>
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Users />} />
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path='/' element={<Users />} />
 
-          <Route path='/places' element={<Places />} />
+            <Route path='/places' element={<Places />} />
 
-          <Route path='*' element={<Navigate to='/' />} />
-        </Routes>
+            <Route path='*' element={<Navigate to='/' />} />
+          </Routes>
+        </main>
       </BrowserRouter>
     </div>
   );
--- src/ui/shared/components/navigation/MainNavigation.tsx
+++ src/ui/shared/components/navigation/MainNavigation.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './MainNavigation.css';
 
+import MainHeader from '@app/ui/shared/components/navigation/MainHeader';
+
 export default function MainNavigation(): JSX.Element {
-  return <></>;
+  return (
+    <MainHeader>
+      <button className='main-navigation__menu-btn'>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <h1 className='main-navigation__title'>
+        <Link to='/'>Your Places</Link>
+      </h1>
+      <nav>...</nav>
+    </MainHeader>
+  );
 }
