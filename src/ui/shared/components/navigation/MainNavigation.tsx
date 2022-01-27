19ae7e11b63be26eea9257fec05265ed629c0932
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-01-28 03:39:04 +0400

    Add main navigation.

--- src/ui/shared/components/navigation/MainNavigation.tsx
+++ src/ui/shared/components/navigation/MainNavigation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './MainNavigation.css';
+import NavLinks from './NavLinks';
 
 import MainHeader from '@app/ui/shared/components/navigation/MainHeader';
 
@@ -9,14 +10,16 @@ export default function MainNavigation(): JSX.Element {
   return (
     <MainHeader>
       <button className='main-navigation__menu-btn'>
-        <span></span>
-        <span></span>
-        <span></span>
+        <span />
+        <span />
+        <span />
       </button>
       <h1 className='main-navigation__title'>
         <Link to='/'>Your Places</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 }
