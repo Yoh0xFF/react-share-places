dba35539adb2f230eb6b75e4abb8ee8ea0a05963
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-01-29 02:38:15 +0400

    Add custom buttons.

--- src/ui/places/components/PlaceItem.tsx
+++ src/ui/places/components/PlaceItem.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './PlaceItem.css';
 
 import { Place } from '@app/type/place';
+import Button from '@app/ui/shared/components/form-elements/Button';
 import Card from '@app/ui/shared/components/ui-elements/Card';
 
 interface PlaceItemProps {
@@ -26,9 +27,9 @@ export default function PlaceItem(props: PlaceItemProps): JSX.Element {
         </div>
 
         <div className='place-item__actions'>
-          <button>VIEW ON MAP</button>
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse>VIEW ON MAP</Button>
+          <Button to={`/places/${place.id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
