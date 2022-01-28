04058156ec53f5f117a2f41aee788ec4956ca8ab
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-01-29 01:46:05 +0400

    Add missing class name.

--- src/ui/shared/components/navigation/MainNavigation.tsx
+++ src/ui/shared/components/navigation/MainNavigation.tsx
@@ -35,7 +35,7 @@ export default function MainNavigation(): JSX.Element {
         <h1 className='main-navigation__title'>
           <Link to='/'>Your Places</Link>
         </h1>
-        <nav>
+        <nav className='main-navigation__header-nav'>
           <NavLinks />
         </nav>
       </MainHeader>
