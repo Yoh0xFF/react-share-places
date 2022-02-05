167eab20c95ea0b7c47953f418cc8bd14e8c99c0
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-02-05 22:12:24 +0400

    Add delete confirmation modal.

--- src/ui/places/components/PlaceItem.tsx
+++ src/ui/places/components/PlaceItem.tsx
@@ -14,9 +14,17 @@ interface PlaceItemProps {
 
 export default function PlaceItem(props: PlaceItemProps): JSX.Element {
   const [showMap, setShowMap] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
   const { place } = props;
 
   const toggleMapHandler = () => setShowMap(!showMap);
+
+  const toggleShowConfirmHandler = () => setShowConfirm(!showConfirm);
+
+  const confirmDeleteHandler = () => {
+    setShowConfirm(false);
+    console.log('DELETING...');
+  };
 
   return (
     <>
@@ -32,6 +40,29 @@ export default function PlaceItem(props: PlaceItemProps): JSX.Element {
           <Map center={place.location} zoom={16} />
         </div>
       </Modal>
+
+      <Modal
+        show={showConfirm}
+        onCancel={toggleShowConfirmHandler}
+        header='Are you sure?'
+        footerClassName='place-item__modal-actions'
+        footer={
+          <>
+            <Button inverse onClick={toggleShowConfirmHandler}>
+              CANCEL
+            </Button>
+            <Button danger onClick={confirmDeleteHandler}>
+              CONFIRM
+            </Button>
+          </>
+        }
+      >
+        <p>
+          Do you want to proceed and delete this place? Please note that it
+          can't be undone thereafter.
+        </p>
+      </Modal>
+
       <li className='place-item'>
         <Card className='place-item__content'>
           <div className='place-item__image'>
@@ -49,7 +80,9 @@ export default function PlaceItem(props: PlaceItemProps): JSX.Element {
               VIEW ON MAP
             </Button>
             <Button to={`/places/${place.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button danger onClick={toggleShowConfirmHandler}>
+              DELETE
+            </Button>
           </div>
         </Card>
       </li>
