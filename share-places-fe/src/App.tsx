c889bacd9c6f7bf8c0fb7cf4129f0167b01ded64
Ioram Gordadze <Yoh0xFF@gmail.com>
2022-02-14 03:18:18 +0400

    Add code splitting by lazy loading.

--- share-places-fe/src/App.tsx
+++ share-places-fe/src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import LoadingSpinner from './ui/shared/components/ui-elements/LoadingSpinner';
 import { AuthContext } from './ui/shared/context/auth-context';
 
-import NewPlace from '@app/ui/places/pages/NewPlace';
-import UpdatePlace from '@app/ui/places/pages/UpdatePlace';
-import UserPlaces from '@app/ui/places/pages/UserPlaces';
 import MainNavigation from '@app/ui/shared/components/navigation/MainNavigation';
 import { useAuth } from '@app/ui/shared/hooks/auth-hook';
-import Auth from '@app/ui/users/pages/Auth';
-import Users from '@app/ui/users/pages/Users';
+
+const Auth = React.lazy(() => import('./ui/users/pages/Auth'));
+const Users = React.lazy(() => import('./ui/users/pages/Users'));
+const UserPlaces = React.lazy(() => import('./ui/places/pages/UserPlaces'));
+const NewPlace = React.lazy(() => import('./ui/places/pages/NewPlace'));
+const UpdatePlace = React.lazy(() => import('./ui/places/pages/UpdatePlace'));
 
 function App(): JSX.Element {
   const { isLoading, userId, token, login, logout } = useAuth();
@@ -53,7 +55,17 @@ function App(): JSX.Element {
         >
           <BrowserRouter>
             <MainNavigation />
-            <main>{routes}</main>
+            <main>
+              <Suspense
+                fallback={
+                  <div className='center'>
+                    <LoadingSpinner />
+                  </div>
+                }
+              >
+                {routes}
+              </Suspense>
+            </main>
           </BrowserRouter>
         </AuthContext.Provider>
       )}
